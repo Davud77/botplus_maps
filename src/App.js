3c0d3c2927a8992cc6c2990cb5496fe871018979
Davud77 <77davud77@gmail.com>
2024-05-11 10:51:41 +0300

    add src

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Home';
+import UploadPage from './UploadPage';
+import MapPage from './MapPage';
+import LoginPage from './LoginPage';
+import ProfilePage from './ProfilePage';
+import './styles.css';
+
+
+
+
+function App() {
+  const isAuthenticated = sessionStorage.getItem('auth');
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isAuthenticated ? <Home /> : <Navigate replace to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/upload" element={isAuthenticated ? <UploadPage /> : <Navigate replace to="/login" />} />
+        <Route path="/map" element={isAuthenticated ? <MapPage /> : <Navigate replace to="/login" />} />
+        <Route path="/profile" element={isAuthenticated ? <ProfilePage /> : <Navigate replace to="/login" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/UploadPage.js
@@ -0,0 +1,78 @@
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import Header from './Header'; // Импортируем Header
+
+const UploadPage = () => {
+  const [files, setFiles] = useState([]);
+  const [tags, setTags] = useState('');
+  const fileInputRef = useRef(null);
+
+  const onFileChange = (event) => {
+    // Фильтрация выбранных файлов, чтобы включать только .jpg
+    const filteredFiles = Array.from(event.target.files).filter(file => file.type === "image/jpeg");
+    if(filteredFiles.length !== event.target.files.length) {
+      alert('Можно загружать только файлы с расширением .jpg');
+    }
+    setFiles(filteredFiles);
+  };
+
+  const onTagsChange = (event) => {
+    setTags(event.target.value);
+  };
+
+  const onFileUpload = async () => {
+    const formData = new FormData();
+    files.forEach(file => {
+      formData.append("files", file);
+    });
+    formData.append("tags", tags);
+
+    try {
+      const response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const result = await response.json();
+      alert(`Загрузка завершена. Успешно: ${result.successful_uploads}, Ошибок: ${result.failed_uploads}`);
+    } catch (error) {
+      console.error('Ошибка при загрузке:', error);
+      alert('Произошла ошибка при загрузке файлов.');
+    }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <div className="background">
+      <Header /> {/* Добавляем Header в начало страницы */}
+      <div className="centered-container">
+        <h1>Массовая загрузка панорам</h1>
+        <textarea
+          value={tags}
+          onChange={onTagsChange}
+          placeholder="Введите теги через запятую"
+          rows="4"
+          cols="50"
+          className="input-tags"
+        />
+        <div className="button-container">
+          <input
+            type="file"
+            multiple
+            accept=".jpg"
+            onChange={onFileChange}
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+          />
+          <button className="button" onClick={handleFileInputClick}>Выбрать файлы</button>
+          <button onClick={onFileUpload} className="button">Загрузить</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UploadPage;
