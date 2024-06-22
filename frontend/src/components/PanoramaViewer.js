d815416268ef5032f16e19fa0ddb8f5a8efe7c36
Davud77 <77davud77@gmail.com>
2024-06-22 19:19:08 +0300

    оптимизация информация о точке

--- frontend/src/components/PanoramaViewer.js
+++ frontend/src/components/PanoramaViewer.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import Marzipano from 'marzipano';
+import PointInfo from './PointInfo'; // Импортируем PointInfo
 import '../assets/css/styles.css';
-
 
 const PanoramaViewer = ({ imageUrl, isExpanded }) => {
   const viewerRef = useRef(null);
@@ -73,14 +73,7 @@ const PanoramaViewer = ({ imageUrl, isExpanded }) => {
           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
         </svg>
       </button>
-      {showPointInfo && (
-        <div className="layers-menu">
-          {/* Ваше содержимое для окна pointinfo */}
-          <h2>Информация о точке</h2>
-          <p>Здесь можно разместить любую информацию или настройки</p>
-          
-        </div>
-      )}
+      {showPointInfo && <PointInfo />}
     </div>
   );
 };
---
+++ frontend/src/components/PointInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PointInfo = () => {
+  return (
+    <div className="layers-menu">
+      <h2>Информация о точке</h2>
+      <p>Здесь можно разместить любую информацию или настройки</p>
+    </div>
+  );
+};
+
+export default PointInfo;
