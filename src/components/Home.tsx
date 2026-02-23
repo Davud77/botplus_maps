c37ff6b63416d1183509487555aa3951357e6f83
Davud Musagadziev <77davud77@gmail.com>
2026-02-23 14:59:27 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,13 @@
-// src/components/Home.tsx
 import React from 'react';
 import Header from './Header';
 
 const Home: React.FC = () => {
   return (
-    <div className="background full-screen-video">
+    <div className="home-container">
       <Header />
-      <video autoPlay loop muted style={{ position: 'absolute', width: '100vw', height: '100vh', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', objectFit: 'cover' }}>
-        <source src="/videos/banner.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div className="version">
+        0.0.1
+      </div>
     </div>
   );
 };
